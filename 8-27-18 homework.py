a4d74f654fa9a24e2875a1174864fc7b326bb0bc
jlovjoy1996 <42555228+jlovjoy1996@users.noreply.github.com>
2018-08-27 15:10:02 -0500

    homework 1

---
+++ 8-27-18 homework.py
@@ -0,0 +1,12 @@
+#asks for name and age
+first = input('enter your first name: ') 
+last = input('enter your last name: ')
+age = input('enter your age: ')
+#makes a sentence with the info
+print ('hey {0} {1} your are {2} years old.'.format (first, last, age))
+# ask for numbers
+x = float(input('enter a number'))
+y = float(input('enter a second number'))
+# multiplys and makes a sentance
+z = x * y
+print ('the producer of {0} times {1} is {2}'.format(x, y, z))
