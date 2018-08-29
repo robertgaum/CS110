e4f839f156a3a09e20923473cc137a21287e10d5
gavinrich300 <42555245+gavinrich300@users.noreply.github.com>
2018-08-29 18:48:54 -0400

    Add files via upload

---
+++ Lab1.py
@@ -0,0 +1,70 @@
+print ("Lab Assignment 1")
+
+print ("Gavin Richardson")
+
+print ("\'"""'""""'''""''"'")
+
+Name = input("Enter your name:")
+print (f"Hello, {Name}!")
+
+Number1 = 6
+print (type (Number1))
+
+Number2 = 4.25
+print (type (Number2))
+
+hello = ("hello!")
+print (type (hello))
+
+boool = True
+print (type (boool))
+
+Set1 = [1,2,3,4]
+print (type(Set1))
+print (Set1[3])
+
+Set2 = [1,2,3,4]
+print (type (Set2))
+print (Set2[1])
+
+Profile = {'Name':'Zara','Age': 7, 'Class':'First'}
+print (type (Profile))
+print (Profile['Age'])
+
+x = (2 * 6)
+print (x)
+
+x = (5 + 6)
+print (x)
+
+x = (2 - 8)
+print (x)
+
+x = (-3 - 9)
+print (x)
+
+x = (4 / 2)
+print (x)
+
+x = (2 / 4)
+print (x)
+
+x = (5 % 2)
+print (x)
+
+x = (5 % 3)
+print (x)
+
+x = (6 % 2)
+print (x)
+
+x = (2 ** 5)
+print (x)
+
+x = 25
+print (id (x))
+
+y = 25
+print (id (y))
+
+print ("The way python manages its memory is that it uses the same id for each variable that is assigned to an identical number.")
