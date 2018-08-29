36660631614fc62dff9c3f843ff04b0e97451f94
jlovjoy1996 <42555228+jlovjoy1996@users.noreply.github.com>
2018-08-29 16:17:06 -0500

    Add files via upload

---
+++ 8-29-18.py
@@ -0,0 +1,35 @@
+print ('lab assingment 1')
+print ('Jesse Lovejoy')
+
+name = input('what is your name')
+print ('hellow {0}'.format(name))
+x = 6
+print (type(x))
+y = 4.25
+print (type(y))
+h = "hellow"
+print (type(h))
+t = True
+print (type(t))
+l = {1,2,3,4}
+print (type(l))
+t = (1,2,3,4)
+print (type(t))
+print (t[0])
+q = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print (type(q))
+print (q['Age'])
+a = 2*6
+b = 5+6
+c = 2-8
+d = -3-9
+e = 4/2
+f = 2/4
+g = 5%3
+k = 6%2
+m = 2**5
+v = 25
+w = 25
+print (id(w))
+print (id(v))
+print ('to save memory')
