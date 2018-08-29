367489ffc6860ea3a78156a191d6a66852472977
gavinrich300 <42555245+gavinrich300@users.noreply.github.com>
2018-08-29 14:58:39 -0400

    Add files via upload

---
+++ Homework1.py
@@ -0,0 +1,10 @@
+Name=input("What is your first and last name?")
+Age=input("What is your age?")
+print (f"Hello, {Name}! You are {Age} years old!")
+
+Number1str=input("Enter a number:")
+Number1 = int(Number1str)
+Number2str=input("Enter another Number:")
+Number2 = int(Number2str)
+print (f"The product of those numbers is: {Number1 * Number2}!")
+#this took me an hour to figure out D:
