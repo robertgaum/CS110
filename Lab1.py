ad397dd2a0f2dcb7c49a66abd57a56257a8972b1
robertgaum <34657630+robertgaum@users.noreply.github.com>
2018-08-30 07:48:12 -0400

    Lap 1 Solution

---
+++ Lab1.py
@@ -0,0 +1,47 @@
+print("Lab Assignment 1")
+print("Robert Gaum")
+print("'\"\"\"'\"\"\"\"'''\"\"''\"")
+print("Please enter your name: ")
+print("Hello, "+ input())
+a = 6
+print(type(a))
+b = 4.25
+print(type(b))
+c = "Hello!"
+print(type(c))
+d = True
+print(type(d))
+e = [1,2,3,4]
+print(type(e))
+print(e[3])
+f = (1,2,3,4)
+print(type(f))
+print(f[1])
+g = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+print(type(g))
+print(g['Age'])
+h,i = 2, 6
+print(h*i)
+h,i = 5, 6
+print(h+i)
+h,i = 2, 8
+print(h-i)
+h,i = -3, 9
+print(h-i)
+h,i = 4, 2
+print(h/i)
+h,i = 2, 4
+print(h/i)
+h,i = 5, 2
+print(h%i)
+h,i = 5, 3
+print(h%i)
+h,i = 6, 2
+print(h%i)
+h,i = 2, 5
+print(h**i)
+j = 25
+k = 25
+print(id(j))
+print(id(k))
+print("Python is memory efficient in the way that two variables with the exact same value and value type, point to the same spot in memory.")
