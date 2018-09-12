4535461e9a7acc48d61bd58e87136dd1cb053dd4
robertgaum <34657630+robertgaum@users.noreply.github.com>
2018-09-12 08:12:22 -0400

    Week 3 Homework Solution

---
+++ Week3 Homework Solution.py
@@ -0,0 +1,12 @@
+phrase = 'The quick brown fox jumps over the lazy dog.'
+
+print (phrase.upper())
+
+print (phrase.lower())
+
+print (phrase.title())
+
+print (phrase.replace('o','x'))
+
+split = phrase.split()
+print (",".join(split))
