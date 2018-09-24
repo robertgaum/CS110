279abf0b8724fd29b9d4ad19b3525e5b2cb5350e
gavinrich300 <42555245+gavinrich300@users.noreply.github.com>
2018-09-24 13:51:56 -0400

    Update Homework5.py

--- Homework5.py
+++ Homework5.py
@@ -9,11 +9,24 @@ while whileNum <= 9:
     print (whileNum)
 
 #I create a list of numbers and print the minimum, maximum and average of the values in the array.
-List = (3,6,1,9,15,11,13,7,2)
-Max_value = max(List)
-Min_value = min(List)
-avg_value = sum(List)/len(List)
+numbers = (3,6,1,9,15,11,13,7,2)
+biggest = numbers[0]
+smallest = numbers[0]
 
-print (f">> Your maximum Value is: {Max_value}")
-print (f">> Your minimum Value is: {Min_value}")
-print (f">> Your average Value is: {avg_value}")
+for bigs in range(1,len(numbers)):
+    if numbers[bigs] > biggest:
+        biggest = numbers [bigs]
+print(">> The max number is", biggest)
+
+for smalls in range(1,len(numbers)):
+    if numbers[smalls] < smallest:
+        smallest = numbers [smalls]
+print(">> The min number is", smallest)
+
+average = 0
+Sum = 0
+for n in numbers:
+    Sum = (Sum + n)
+    average = Sum / len(numbers)
+
+print (f">> The average number is: {average}")
